1d6648395d4b61f7839a8a82918f18bc4731e3bc
Keon <Keon.ke@everfortune.ai>
2023-05-18 13:14:14 +0800

    style + env

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 const App = () => {
+    const api_path = import.meta.env.VITE_API_PATH;
+
+    useEffect(() => {
+        (async () => {
+            const res = await axios.get(`${import.meta.env.VITE_API_URL}/v2/api/${api_path}/products/all`);
+            console.log(res);
+        })();
+    }, []);
+
     return (
         <>
-            <h1>AI 工具王</h1>
+            <h1 className="text-primary">React 最終挑戰</h1>
         </>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.scss";
+import "./style/index.scss";
+import axios from "axios";
+// axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
